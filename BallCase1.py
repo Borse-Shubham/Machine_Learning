fb785ac85c9217b957f960c71fd55998530c5ab2
Borse-Shubham <shubhamborse0911@gmail.com>
2023-10-01 21:22:57 +0530

    Kids Toy Demo Case study

--- BallCase1.py
+++ BallCase1.py
@@ -1,16 +1,18 @@
-from sklearn import *
+from sklearn import tree
 
 def main():
-    print("Ball Classification Case study")
+    print("Ball Classification Case Study")
 
-    BallFeatures =[[35,"Rough"],[47,"Rough"],[90,"Smooth"],[48,"Rough"],[90,"Smooth"]] 
+    # Load the data
+    BallFeatures = [[35,"Rough"],[47,"Rough"],[90,"Smooth"],[48,"Rough"],[90,"Smooth"],[35,"Rough"], [92,"Smooth"],[35,"Rough"],[35,"Rough"],[35,"Rough"], [96,"Smooth"], [43,"Rough"],[110, "Smooth"], [35,"Rough"],[95, "Smooth"]]
 
-    Labels = ["Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket","Tennis","Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket"]
+    Labels = ["Teenis", "Tennis", "Cricket","Tennis", "Cricket", "Tennis","Cricket", "Tennis","Tennis", "Tennis","Cricket", "Tennis", "Cricket","Tennis","Cricket"]
 
-    obj = tree.DecisionTreeClassifier()
+    obj = tree.DecisionTreeClassifier()     # Decide the algorithm
 
-    obj = obj.fit(BallFeatures,Labels)
+    obj = obj.fit(BallFeatures,Labels)      # Train the model
 
-    print(obj.predict([36,"Rough"],[91,"Smooth"]))
-if __name__=="__main__":
+    print(obj.predict([[36,"Rough"], [91,"Smooth"]]))   # Test the model
+
+if __name__ == "__main__":
     main()
--- BallCase2.py
+++ BallCase2.py
@@ -1,17 +1,18 @@
-from sklearn import *
+from sklearn import tree
 
 def main():
-    print("Ball Classification Case study")
+    print("Ball Classification Case Study")
 
-    BallFeatures =[[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,1],[35,1],[35,1],[96,0],[43,1],[110,0],[35,1],[95,0]] 
+    # Load the data
+    BallFeatures = [[35,1],[47,1],[90,0],[48,1],[90,0],[35,1], [92,0],[35,1],[35,1],[35,1], [96,0], [43,1],[110,0], [35,1],[95,0]]
 
-    Labels = ["Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket","Tennis","Tennis","Tennis","Cricket","Tennis","Cricket","Tennis","Cricket"]
+    Labels = ["Teenis", "Tennis", "Cricket","Tennis", "Cricket", "Tennis","Cricket", "Tennis","Tennis", "Tennis","Cricket", "Tennis", "Cricket","Tennis","Cricket"]
 
-    obj = tree.DecisionTreeClassifier()
+    obj = tree.DecisionTreeClassifier()     # Decide the algorithm
 
-    obj = obj.fit(BallFeatures,Labels)
+    obj = obj.fit(BallFeatures,Labels)      # Train the model
 
-    print(obj.predict([36,0],[91,1]))
-if __name__=="__main__":
+    print(obj.predict([[39,1], [89,0]]))   # Test the model
+
+if __name__ == "__main__":
     main()
-    
